b0a6a1f6e3455afcb6b6cd6646cfc0e88098030e
Sergio Borne <sergio.borne@fleetmatics.com>
2016-08-23 22:58:00 +0100

    Error syncing git

---
+++ app/src/main/java/com/sergioborne/wallatest/app/Constants.java
@@ -0,0 +1,10 @@
+package com.sergioborne.wallatest.app;
+
+public class Constants {
+  public static final String API_BASE_URI = "http://gateway.marvel.com";
+
+  public static final String PUBLIC_KEY = "public-key";
+  public static final String PRIVATE_KEY = "private-key";
+
+  public static final int CAPTAIN_AMERICA_ID = 1009220;
+}
